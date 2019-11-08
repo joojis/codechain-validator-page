3a5cba67ed5e91b97bd1896cf0b0cedd01ab465b
JinGyeong Jeong <jjg@codechain.io>
2019-11-08 17:49:06 +0900

    Improve the profile

---
+++ src/components/profile-joojis/index.js
@@ -0,0 +1,69 @@
+import React from "react"
+import styled from "styled-components"
+
+import Image from "./image"
+
+const ImageWrapper = styled.div`
+  width: 180px;
+  height: 180px;
+`
+
+const Lato = styled.div`
+  font-family: Lato;
+`
+
+const Name = styled.span`
+  font-weight: bold;
+  font-size: 1.5rem;
+`
+
+const RowSpaceBetween = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const GitHubLink = styled.a`
+  &:hover {
+    color: #666;
+  }
+  color: #333;
+  font-family: sans-serif;
+  font-weight: bold;
+  text-decoration: none;
+`
+
+const TwitterLink = styled.a`
+  &:hover {
+    color: #58c1ff;
+  }
+  color: #38a1f3;
+  font-family: Helvetica;
+  font-weight: bold;
+  text-decoration: none;
+`
+
+const Profile = () => {
+  const name = "JinGyeong Jeong"
+  const description = "JinGyeong Jeong is a core developer of CodeChain"
+  const githubLink = "https://github.com/joojis/"
+  const twitterLink = "https://twitter.com/jjgjoojis"
+  return (
+    <RowSpaceBetween>
+      <Lato>
+        <Name>{name}</Name>
+        <p style={{ marginTop: "1.5rem", marginBottom: "2.5rem" }}>
+          {description}
+        </p>
+        <GitHubLink href={githubLink}>GitHub</GitHubLink>
+        <br />
+        <TwitterLink href={twitterLink}>Twitter</TwitterLink>
+      </Lato>
+      <ImageWrapper>
+        <Image />
+      </ImageWrapper>
+    </RowSpaceBetween>
+  )
+}
+
+export default Profile
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,18 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import Profile from "../components/profile"
+import Profile from "../components/profile-joojis"
 import Validator from "../components/validator"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="jNode the CodeChain validator service" />
     <Validator />
-    <Profile
-      name="JinGyeong Jeong"
-      description=""
-      image={
-        <div style={{ maxWidth: `100px`, marginBottom: `1.45rem` }}>
-          <Image />
-        </div>
-      }
-    />
+    <div style={{ height: 200 }}></div>
+    <Profile />
+    <div style={{ height: 200 }}></div>
+    <hr />
   </Layout>
 )
 
