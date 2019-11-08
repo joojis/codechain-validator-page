69e02b13f962b2c804ca0f10f1008d4ab8a8c1d2
JinGyeong Jeong <jjg@codechain.io>
2019-11-08 18:21:54 +0900

    Improve the validator component

--- src/components/validator.js
+++ src/components/validator.js
@@ -1,17 +1,44 @@
 import React from "react"
+import styled from "styled-components"
 
 import PlatformAddress from "./platform-address"
+
+const Lato = styled.div`
+  font-family: Lato;
+`
+
+const Monospace = styled.div`
+  font-family: monospace;
+`
+
+const ActiveColor = styled.span`
+  color: #05aad1;
+  font-weight: bold;
+`
 
 const address = "cccqy6qzunhrdq8trsv299kjhnyljq0j2jcmqkhsftc"
 
 const Validator = () => (
-  <>
-    <div>
-      address: <PlatformAddress address={address} />
-    </div>
-    <div>Node Settings: ...</div>
-    <div>ccstake delegate --delegatee {address}</div>
-  </>
+  <Lato>
+    <h2>Mainnet</h2>
+    <table>
+      <tr>
+        <td>Status</td>
+        <td>
+          <ActiveColor>Running</ActiveColor>
+        </td>
+      </tr>
+      <tr>
+        <td>Address</td>
+        <td>
+          <PlatformAddress address={address} />
+        </td>
+      </tr>
+    </table>
+    <Monospace>
+      <center>ccstake delegate --delegatee {address}</center>
+    </Monospace>
+  </Lato>
 )
 
 export default Validator
