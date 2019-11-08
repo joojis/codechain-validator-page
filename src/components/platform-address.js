fd89bb227e284052b727fcfbb956c2be30f2f659
JinGyeong Jeong <jjg@codechain.io>
2019-11-08 17:01:44 +0900

    Set monospace for addresses

--- src/components/platform-address.js
+++ src/components/platform-address.js
@@ -1,8 +1,13 @@
 import React from "react"
+import styled from "styled-components"
+
+const Monospace = styled.span`
+  font-family: monospace;
+`
 
 const PlatformAddress = ({ address }) => (
   <a href={`https://explorer.codechain.io/addr-platform/${address}`}>
-    {address}
+    <Monospace>{address}</Monospace>
   </a>
 )
 
