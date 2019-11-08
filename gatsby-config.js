eab082584aededac448132833e0a29d9a596c766
JinGyeong Jeong <jjg@codechain.io>
2019-11-08 17:01:01 +0900

    Add the styled-components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   pathPrefix: "/codechain-validator-page",
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
